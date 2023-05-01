52665af4ce5954eb3561d268e2badf226a72266e
kwakchaewon <ksh03003@naver.com>
2023-05-01 15:29:40 +0900

    Learn django -Implement search function through form

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,2 +1,6 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
+    path('', views.insta_post, name = "insta_post")
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def insta_post(request):
+    qs = Post.objects.all()
+    q = request.GET.get('q','')
+    
+    if q:
+        qs = qs.filter(message__icontains=q)
+    
+    print(q)
+    
+    # instagram/templates/instagram/post_list.html
+    return render(request, 'insta_post.html', {
+        'post_list':qs,
+        'q':q,
+    })
