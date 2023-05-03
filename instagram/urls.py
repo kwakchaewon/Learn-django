6c31ec4bce8a2a26786c22f87b1bdecfbe82c95a
kwakchaewon <ksh03003@naver.com>
2023-05-03 19:57:07 +0900

    fixed untracked files

--- instagram/urls.py
+++ instagram/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.insta_post, name = "insta_post"),
     path('/<int:pk>/', views.post_detail,name='post_detail'),
     path('/post_list/', views.post_list, name='post_list'),
-    path('/read_excel', views.read_excel)
+    path('/read_excel', views.read_excel),
 ]
